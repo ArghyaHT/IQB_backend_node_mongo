ee0bfccd44e87196127a57454eb79e12d7587194
Arghya211994 <arghya@himanstech.com>
2023-12-02 18:26:52 +0530

    twilio added for sending verification code

--- utils/mobileMessageSender.js
+++ utils/mobileMessageSender.js
@@ -1,36 +1,28 @@
-const axios = require('axios');
+const twilio = require('twilio');
 
-// Function to send verification code
-const sendVerificationCodeToMobile = async(mobileNumber, verificationCode)=> {
-  const apiKey = 'BY6qBfYt0GwwXu62WRancbNX4FUtvmIII6BvvBn1FUhw7cGQYR3eLlDjPn7T';
-  const senderId = 'FSTSMS';
-  const message = `Your verification code is: ${verificationCode}`;
+// Function to send verification code using Twilio
+const sendVerificationCodeToMobile = async (mobileNumber, verificationCode) => {
+  const accountSid = 'YOUR_ACCOUNT_SID'; // Your Twilio account SID
+  const authToken = 'YOUR_AUTH_TOKEN'; // Your Twilio auth token
+  const twilioPhoneNumber = 'YOUR_TWILIO_PHONE_NUMBER'; // Your Twilio phone number
 
   try {
-    const response = await axios.post(
-      'https://www.fast2sms.com/dev/bulkV2',
-      {
-        sender_id: senderId,
-        message: message,
-        language: 'english',
-        route: 'otp',
-        numbers: mobileNumber,
-      },
-      {
-        headers: {
-          Authorization: apiKey,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+    const client = twilio(accountSid, authToken);
 
-    console.log(response.data);
-    return response.data;
+    const message = await client.messages.create({
+      body: `Your verification code is: ${verificationCode}`,
+      from: twilioPhoneNumber,
+      to: mobileNumber,
+    });
+
+    console.log('Message sent:', message.sid);
+    return message;
   } catch (error) {
-    console.error('Error sending verification code:', error.response.data);
+    console.error('Error sending verification code:', error);
     throw new Error('Failed to send verification code');
   }
-}
-module.exports ={
-    sendVerificationCodeToMobile,
-}
+};
+
+module.exports = {
+  sendVerificationCodeToMobile,
+};
