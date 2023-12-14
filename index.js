f735c3f16ec2ee39259691cbc2c925d15d338867
Sumit <meathdev@outlook.com>
2023-12-14 21:39:10 +0530

    Changed Cors API library

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ const appointments = require("./routes/Appointments/appointmentsRoutes.js")
 const students = require("./routes/ImageUploadDemo/student.js")
 
 const rateLimiter = rateLimit({
-	windowMs: 20 * 1000, // 15 minutes
-	limit: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 20 * 1000, // 15 minutes
+  limit: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   message: 'Too many request from this IP.Please try again later',
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-	// store: ... , // Use an external store for more precise rate limiting
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  // store: ... , // Use an external store for more precise rate limiting
 })
 
 connectDB()
@@ -40,15 +40,15 @@ connectDB()
 const app = express()
 
 app.use(cors({
-  // origin:"https://iqb-react-frontend.netlify.app",
-  origin:"http://localhost:5173",
-  credentials:true
+  origin: "https://iqb-react-frontend.netlify.app",
+  // origin:"http://localhost:5173",
+  credentials: true
 }))
 
 app.use(cookieParser())
 app.use(rateLimiter)
 app.use(express.json({ limit: '10mb' }))
-app.use(express.urlencoded({extended:true,limit: '10mb'}))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
 //Image upload =============
 
@@ -65,8 +65,8 @@ if (NODE_ENV != "production") {
 }
 
 app.use(fileUpload({
-  debug:true,
-  useTempFiles:true,
+  debug: true,
+  useTempFiles: true,
   // tempFileDir: path.join(__dirname,"./temp")
 }));
 app.use(express.static("uploads"));
@@ -98,5 +98,5 @@ app.use("/api/appointments", appointments)
 const PORT = 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-  });
+  console.log(`Server started on port ${PORT}`);
+});
