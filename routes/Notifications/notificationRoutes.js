1bf07a4e0b18accb7ee148d4af3b52590d6201be
Arghya211994 <arghya@himanstech.com>
2024-01-10 13:47:22 +0530

    handleProtected route added in notifications

--- routes/Notifications/notificationRoutes.js
+++ routes/Notifications/notificationRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { registerFcmToken, sendNotification, getAllNotifications } = require("../../controllers/notifications/notificationController");
+const { handleProtectedRoute } = require("../../controllers/admin/adminRegisterController");
 
 const router = express.Router();
 
 // Endpoint for sending notifications
-router.route("/send-notification").post(sendNotification)
+router.route("/send-notification").post(handleProtectedRoute, sendNotification)
 
 //Get All Notifications
-router.route("/getAllNotifications").post(getAllNotifications)
+router.route("/getAllNotifications").post(handleProtectedRoute, getAllNotifications)
 
 
 module.exports = router 
