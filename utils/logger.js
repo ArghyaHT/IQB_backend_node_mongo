b2acae041e6991f68bdb707d6d65a82501fce887
ManojGaru <manoj.garu@spicasys.com>
2024-02-10 22:34:38 +0530

    logger integrated

---
+++ utils/logger.js
@@ -0,0 +1,16 @@
+const winston = require('winston');
+
+const { createLogger, transports } = require('winston');
+
+const logger = createLogger({
+    format: winston.format.combine(
+        winston.format.timestamp(), // Add timestamp to logs
+        winston.format.json() // JSON format for logs
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: 'logger.log' }) // Log to file with timestamp
+    ]
+});
+
+module.exports = logger;
