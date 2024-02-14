4ebf9790bce93318a0571d9a4627c9533c741286
Arghya211994 <arghya@himanstech.com>
2024-02-14 17:02:58 +0530

    bug fixed

--- controllers/barber/barberRegisterController.js
+++ controllers/barber/barberRegisterController.js
@@ -1298,7 +1298,7 @@ const updateBarberAccountDetails = async (req, res, next) => {
     }
 
     // Validate password length
-    if (password.length < 8) {
+    if (password && password.length < 8) {
       return res.status(400).json({
         success: false,
         message: "Password must be at least 8 characters long"
