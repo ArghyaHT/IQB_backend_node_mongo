c51beae2b3aeea4a638c18fca869f65b70418683
Arghya211994 <arghya@himanstech.com>
2024-02-27 12:24:01 +0530

    properties to search and limit added for countries and cities

--- controllers/countries/countriesController.js
+++ controllers/countries/countriesController.js
@@ -4,24 +4,31 @@ const city = require("country-state-city").City;
 //GET ALL COUNTRIES
 const getAllCountries = async (req, res) => {
     try {
-        const  {name} = req.query;
+        const { name, page = 1, limit = 10, } = req.query;
 
         let query = {};
 
         // Check if query parameters exist in the request
 
-            if (name) {
-                 query.name = { $regex: new RegExp('^' + name, 'i') }; // Case-insensitive search
-            }
-            // Add more conditions for other query parameters as needed
-        
+        if (name) {
+            query.name = { $regex: new RegExp('^' + name, 'i') }; // Case-insensitive search
+        }
+        // Add more conditions for other query parameters as needed
 
-        const countries = await Country.find(query);
+        const skip = Number(page - 1) * Number(limit);
+
+        const countries = await Country.find(query).skip(skip).limit(Number(limit));
+        const totalCountries = await Country.countDocuments(query);
 
         res.status(200).json({
             success: true,
             message: "Countries retrieved successfully",
-            response: countries
+            response: {
+                countries,
+                totalPages: Math.ceil(totalCountries / Number(limit)),
+                currentPage: Number(page),
+                totalCountries: totalCountries
+            }
         });
     }
     catch (error) {
@@ -30,34 +37,54 @@ const getAllCountries = async (req, res) => {
     }
 }
 
+//GET ALL TIMEZONES
+const getAllTimeZonesByCountry = async(req, res, next) => {
+try{
+const {countryCode} = req.body;
+const country = await Country.findOne({countryCode});
+console.log(country);
+}catch (error) {
+        console.error('Error fetching cities:', error);
+        next(error);
+    }
+}
+
 //GET ALL CITIES
 const getAllCitiesByCountryCode = async (req, res, next) => {
     try {
-        const {countryCode, cityName} = req.query
+        const { countryCode, cityName, page = 1, limit = 20 } = req.query;
 
         let query = {};
+        const cities = city.getAllCities().filter(city => city.countryCode === countryCode);
 
-            // Assuming city.getAllCities() returns an array of cities
-            const cities = city.getAllCities().filter(city => city.countryCode === countryCode);
+        let retrievedCities;
 
-            let retrievedCities;
+        if (cityName) {
+            const searchRegExpCityName = new RegExp('^' + cityName + ".*", 'i');
+            query.name = { $regex: searchRegExpCityName };
 
-            if (cityName) {
-                const searchRegExpCityName = new RegExp('^' + cityName + ".*", 'i');
-                query.name = { $regex: searchRegExpCityName };
-                
-                // Filter city names according to the regex query
-                retrievedCities = cities
-                    .filter(city => searchRegExpCityName.test(city.name))
-            } else {
-                // If no city name is provided, send all city names
-                retrievedCities = cities;
-            }
+            // Filter city names according to the regex query
+            retrievedCities = cities.filter(city => searchRegExpCityName.test(city.name));
+        } else {
+            // If no city name is provided, send all city names
+            retrievedCities = cities;
+        }
+
+        // Pagination
+        const skip = (Number(page) - 1) * Number(limit);
+        const paginatedCities = retrievedCities.slice(skip, skip + Number(limit));
+
+        const totalCities = retrievedCities.length;
 
         res.status(200).json({
             success: true,
             message: "All cities retrieved successfully",
-            response: retrievedCities // Extract city names
+            response: {
+                retrievedCities: paginatedCities,
+                totalPages: Math.ceil(totalCities / Number(limit)),
+                currentPage: Number(page),
+                totalCities: totalCities
+            }
         });
     } catch (error) {
         console.error('Error fetching cities:', error);
@@ -69,5 +96,6 @@ const getAllCitiesByCountryCode = async (req, res, next) => {
 module.exports = {
     getAllCountries,
     getAllCitiesByCountryCode,
+    getAllTimeZonesByCountry,
 
 }
